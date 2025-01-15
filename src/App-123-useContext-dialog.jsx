a280360ab47d16d8b609add9cd7871ff3e7c97c9
GrCOTE7 <grcote7@gmail.com>
2025-01-15 10:33:40 +0100

    Boutn par défaut

--- src/App-123-useContext-dialog.jsx
+++ src/App-123-useContext-dialog.jsx
@@ -1,23 +1,31 @@
 import React from 'react';
-import { useState, createContext, use, createPortal, useEffect } from 'react';
+import { cloneElement, useState, createContext, use, useEffect } from 'react';
+import { createPortal } from 'react-dom';
+import { X } from 'lucide-react';
 
 export default function App() {
   return (
     <div className="flex flex-col gap-4">
       <Dialog>
-        <DialogTrigger />
+        <DialogTrigger>
+          <button className="btn btn-error btn-lg">Open World</button>
+        </DialogTrigger>
         <DialogContent>
-          <h2 className="text-lg font-bold">Hello Guy !</h2>
-          <DialogClose />
+          <h2 className="text-lg font-bold">Hello World!</h2>
+          <DialogClose>
+            <button className="absolute top-4 right-4 flex items-center justify-center size-12 bg-red-400 hover:bg-red-500 animate-in fade-in-50 animate-out fade-out-50 duration-700 rounded-md text-black">
+              <X size={18} />
+            </button>
+          </DialogClose>
         </DialogContent>
       </Dialog>
 
       <Dialog>
-        <DialogTrigger />
+        <DialogTrigger>Open FR</DialogTrigger>
         <DialogContent>
           <h2 className="text-lg font-bold">Salut !</h2>
           <input className="input input-bordered" type="text" />
-          <DialogClose />
+          <DialogClose>Fermer</DialogClose>
         </DialogContent>
       </Dialog>
     </div>
@@ -35,7 +43,7 @@ const useDialog = () => {
 };
 
 const Dialog = ({ children }) => {
-  const [open, setOpen] = useState(1);
+  const [open, setOpen] = useState(0);
 
   return (
     <DialogContext.Provider
@@ -54,52 +62,58 @@ const DialogContent = ({ children }) => {
 
   if (!context.open) return;
 
-  return (
-      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black-50 backdrop-blur-sm animate-in fade-in-50 animate-out fade-out-50 duration-1000">
-        <div className="card w-96 bg-base-200 shadow-xl animate-in fade-in-50 border-2 border-gray-700 hover:bg-indigo-500 duration-1000 hover:scale-110">
-          <div className="card-body">{children}</div>
-        </div>
+  return createPortal(
+    <div className="fixed inset-0 z-50 flex items-center justify-center bg-black-50 backdrop-blur-sm animate-in fade-in-50 animate-out fade-out-50 duration-1000">
+      <div className="card w-96 bg-base-200 shadow-xl animate-in fade-in-50 border-2 border-gray-700 hover:bg-orange-950 duration-700 hover:scale-105">
+        <div className="card-body">{children}</div>
       </div>
+    </div>,
+    document.body
   );
 };
 
-const DialogTrigger = () => {
+const DialogTrigger = ({ children }) => {
   const context = useDialog();
 
+  if (children && typeof children !== 'string') {
+    return cloneElement(children, {
+      onClick: (e) => {
+        context.setOpen(true);
+        // children.props.onClick(e);
+      },
+      style: {
+        justifyContent: 'left',
+        textAlign: 'left',
+        paddingLeft: 350,
+      },
+    });
+  }
+
   return (
-    <>
-      <button
-        className="btn btn-primary btn-lg"
-        onClick={() => {
-          const modal = document.getElementById('modalContent');
-          if (modal instanceof HTMLDialogElement) {
-            modal.showModal();
-          }
-          context.setOpen(true);
-        }}
-      >
-        Open
-      </button>
-    </>
+    <button className="btn btn-primary">{children} (Bouton par défaut)</button>
   );
 };
-const DialogClose = () => {
+
+const DialogClose = ({ children }) => {
   const context = useDialog();
 
+  if (children && typeof children !== 'string') {
+    return cloneElement(children, {
+      onClick: (e) => {
+        context.setOpen(false);
+        // children.props.onClick(e);
+      },
+    });
+  }
+
   return (
-    <>
-      <button
-        className="btn btn-primary btn-lg"
-        onClick={() => {
-          const modal = document.getElementById('modalContent');
-          if (modal instanceof HTMLDialogElement) {
-            modal.showModal();
-          }
-          context.setOpen(false);
-        }}
-      >
-        Close
-      </button>
-    </>
+    <button
+      className="btn btn-primary btn-lg"
+      onClick={() => {
+        context.setOpen(false);
+      }}
+    >
+      {children}
+    </button>
   );
 };
